ef05b25d9bb21078077d46c2620dbd46b8848583
Andy Bell <bell.andy.m@gmail.com>
2017-05-17 10:25:34 -0400

    reorganize figs folder, new TAF figs

--- figs/batch_figures/output_settings.R
+++ figs/batch_figures/output_settings.R
@@ -2,9 +2,8 @@
 source("scripts/helper_functions.R")
 source("scripts/figures.R")
 
-# dataframe to use for generating the plots
-data <- readRDS("data/full_20170509/data_both_wy_20170509.rds")
-data <- data %>% filter(!model == 'itrc_co') # remove the CO results from ITRC
+# data to use for generating the plots
+dsa_legal_data <- readRDS("data/dsa_legal/dsa_legal_5methods_20170517.rds")
 
 # list of crops to generate
 crops_2_gen <- crop_list()
@@ -19,6 +18,7 @@ methods_2_gen <- methods_2_gen[!methods_2_gen %in% c("CalSIMETAW", "DETAW")]
 # region of interest (usually dsa or legal)
 aoi <- "dsa"
 
+# water years
 water_years <- c(2015, 2016)
 
 # footer 
--- scripts/figures/sum_TAFxModel_dsa_legal.R
+++ scripts/figures/sum_TAFxModel_dsa_legal.R
@@ -6,22 +6,34 @@ sum_TAFxModel_dsa_legal <- function(data, wy){
     dplyr::summarise(sum_af=sum(crop_acft)) %>% filter(wateryear == wy)
   
   # manually chuck results for models that don't conver legal delta
-  af_crops_filter <- af_crops %>% filter(model!="eto") %>%
-    filter(model!="calsimetaw" & model!="detaw" & model!="sims" | region!="legal")
+  af_crops_filter <- af_crops %>% 
+    filter(model!="eto") %>% # removes spatial cimis ETo 
+    filter(model!="calsimetaw" & model!="detaw" & model!="sims" | region!="legal") # these models don't cover legal delta
   
   # modify axis units (acre-ft -> thousands acre-feet)
   axis_units <-function(x){
     x/1000
   }
   
+  # calculate the mean value for all the groups for the dsa regions and the legal delta
+  region_averages <- af_crops_filter %>% 
+    dplyr::group_by(region) %>%
+    dplyr::summarise(avg=mean(sum_af), median=median(sum_af))
+  
+  dsa_intercept <- region_averages$avg[region_averages$region=='dsa']
+  legal_intercept <- region_averages$avg[region_averages$region=='legal']
+  
+  
   p <- ggplot(af_crops_filter, aes(x=model, y=sum_af, colour=region))+
-    geom_point(size=2) +
+    geom_point(size=3.5) +
     ggtitle(paste("Acre-feet totals for crops summed over the", wy ,"water year")) +
     scale_y_continuous(labels = axis_units, limits = c(1000000, 2000000)) +
     ylab("Thousands Acre-Feet") +
-    scale_x_discrete(labels=c("CalSIMETAW", "DETAW", "DisALEXI", "ITRC", "SIMS", "UCD-METRIC", "UCD-PT"))+
+    scale_x_discrete(limit=c("calsimetaw", "detaw", "disalexi", "itrc", "sims", "ucdmetric", "ucdpt"), labels=methods_named_list)+ 
     scale_color_manual(labels = c("Delta Service Area", "Legal Delta"), 
                        values = c('#3366cc', "#dc3912")) +
+    geom_hline(aes(yintercept=dsa_intercept), colour="#3366cc", linetype="dashed", size=1.25, alpha=0.5)+
+    geom_hline(aes(yintercept=legal_intercept), colour="#dc3912", linetype="dashed", size=1.25, alpha=0.5)+
     theme_bw() +  
     theme(panel.border = element_blank(),
           panel.grid.major = element_blank(),
@@ -32,6 +44,8 @@ sum_TAFxModel_dsa_legal <- function(data, wy){
           legend.direction="horizontal", # orientation of legend
           legend.title= element_blank(), # no title for legend
           legend.key.size = unit(0.5, "cm"), # size of legend
-          axis.line.x = element_line(color="black", size = 1),
-          axis.line.y = element_line(color="black", size = 1)) # manually add in axis
+          axis.line = element_line(color="black", size = 1),
+          axis.text=element_text(size=12),
+          axis.title=element_text(size=14, face="bold")) # manually add in axis
+  
 }
