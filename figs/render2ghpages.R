2810d2a2012185531d86db6f7cc29b2a083a4afd
Andy Bell <bell.andy.m@gmail.com>
2017-05-19 15:36:21 -0400

    figures with updated itrc

---
+++ figs/render2ghpages.R
@@ -0,0 +1,5 @@
+# render the figures.Rmd to the docs folder
+# the docs folder pushes to gh-pages
+# https://ssj-delta-cu.github.io/ssj-graphs-r/report-figures.html
+
+rmarkdown::render('figs/figures.Rmd', 'html_document', output_file = 'report-figures.html', output_dir = 'docs')
