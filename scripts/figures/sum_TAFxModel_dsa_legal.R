275260a8e9d8b176580a3595234d687d8fb5a21b
Andy Bell <bell.andy.m@gmail.com>
2017-05-19 12:16:17 -0400

    TAF with updated sims

--- scripts/figures/sum_TAFxModel_dsa_legal.R
+++ scripts/figures/sum_TAFxModel_dsa_legal.R
@@ -15,19 +15,49 @@ sum_TAFxModel_dsa_legal <- function(data, wy){
     x/1000
   }
   
+  # sims doesn't include wet herbaceous or semi ag so we'll add a point that represents the average of all methods for herbaceous/sub irrigated pasture.
+  
+  # total acre-feet for sims for these two classes (to subtract from the total)
+  sims_est_wetherb_semi_ag <- data %>% filter(model=="sims", region=='dsa', wateryear==wy, level_2 %in% c(2008, 913)) %>%
+    dplyr::summarise(sum_wetherb_semiag=sum(crop_acft))
+  
+  # get the average for the rest of the models for the two classes
+  mean_est_wetherb_semi_ag <- data %>% filter(!model=="sims", !model=="eto") %>%
+    filter(region=='dsa', wateryear==wy, level_2 %in% c(2008, 913)) %>%
+    dplyr::group_by(model) %>%
+    dplyr::summarise(sum_af=sum(crop_acft)) %>%
+    dplyr::summarise(mean_2classes=mean(sum_af))
+  
+  # get sims TAF results for DSA
+  sims <- af_crops_filter %>% filter(model=="sims")
+  
+  # remove wet herb semi ag from sims total (this will be their original estimate)
+  sims_or <- sims
+  sims_or$sum_af <- sims$sum_af - sims_est_wetherb_semi_ag$sum_wetherb_semiag
+  
+  # add the mean value for the two classes to their original estimate
+  sims_enh <- sims
+  sims_enh$sum_af <- sims$sum_af - sims_est_wetherb_semi_ag$sum_wetherb_semiag + mean_est_wetherb_semi_ag$mean_2classes
+  
+  # remove sims from af_crop_filter and add in the new estimated results
+  af_crop_filter_wo_sims <- af_crops_filter %>% filter(!model=="sims")
+  bind <- bind_rows(af_crop_filter_wo_sims,  sims_enh)
+  
   # calculate the mean value for all the groups for the dsa regions and the legal delta
-  region_averages <- af_crops_filter %>% 
+  region_averages <- bind %>% 
     dplyr::group_by(region) %>%
     dplyr::summarise(avg=mean(sum_af), median=median(sum_af))
   
   dsa_intercept <- region_averages$avg[region_averages$region=='dsa']
   legal_intercept <- region_averages$avg[region_averages$region=='legal']
   
+  # now add in the "sims" orginal
+  bind2 <- bind_rows(bind, sims_or)
   
-  p <- ggplot(af_crops_filter, aes(x=model, y=sum_af, colour=region))+
+  p <- ggplot(bind2, aes(x=model, y=sum_af, colour=region))+
     geom_point(size=3.5) +
     ggtitle(paste("Acre-feet totals for crops summed over the", wy ,"water year")) +
-    scale_y_continuous(labels = axis_units, limits = c(800000, 1500000)) +
+    scale_y_continuous(labels = axis_units, limits = c(1000000, 2000000)) +
     ylab("Thousands Acre-Feet") +
     scale_x_discrete(limit=c("calsimetaw", "detaw", "disalexi", "itrc", "sims", "ucdmetric", "ucdpt"), labels=methods_named_list)+ 
     scale_color_manual(labels = c("Delta Service Area", "Legal Delta"), 
@@ -48,4 +78,8 @@ sum_TAFxModel_dsa_legal <- function(data, wy){
           axis.text=element_text(size=12),
           axis.title=element_text(size=14, face="bold")) # manually add in axis
   
-}
+  # add in the extra data point for sims
+  p2 <- p + annotate("text", x = 'sims', y = sims_enh$sum_af, label = c("   *"), size=8) + 
+    annotate("text", x = 'sims', y = sims_or$sum_af, label = c("   '"), size=8)
+  p2
+  }
