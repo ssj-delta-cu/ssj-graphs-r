516477b624bf276ae83ddd5ef8938da294482122
Andy Bell <bell.andy.m@gmail.com>
2017-07-03 16:52:13 -0400

    add daily info for grid around field stations

--- figs/render2ghpages.R
+++ figs/render2ghpages.R
@@ -3,4 +3,5 @@
 # https://ssj-delta-cu.github.io/ssj-graphs-r/report-figures.html
 
 rmarkdown::render('figs/figures.Rmd', 'html_document', output_file = 'report-figures.html', output_dir = 'docs')
+rmarkdown::render('figs/figures-daily.Rmd', 'html_document', output_file = 'daily-figures.html', output_dir = 'docs')
 
